d19a29d7d6d1795fd1bdce8005ac89ddc5188f59
Jonathan Goode <u01jmg3@gmail.com>
2019-10-02 14:31:26 +0100

    Apply changes required for forked repo

--- src/Commands/ControllersCommand.php
+++ src/Commands/ControllersCommand.php
@@ -61,7 +61,7 @@ class ControllersCommand extends Command
     public function handle()
     {
         $stub = $this->getStub();
-        $files = $this->filesystem->files(base_path('vendor/tcg/voyager/src/Http/Controllers'));
+        $files = $this->filesystem->files(base_path('vendor/u01jmg3/voyager/src/Http/Controllers'));
         $namespace = config('voyager.controllers.namespace', 'TCG\\Voyager\\Http\\Controllers');
 
         $appNamespace = app()->getNamespace();
@@ -103,7 +103,7 @@ class ControllersCommand extends Command
      */
     public function getStub()
     {
-        return $this->filesystem->get(base_path('/vendor/tcg/voyager/stubs/'.$this->stub));
+        return $this->filesystem->get(base_path('/vendor/u01jmg3/voyager/stubs/'.$this->stub));
     }
 
     /**
